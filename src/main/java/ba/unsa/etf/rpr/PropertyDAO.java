42fb14331953b2524054d79bc28aceb413a82b28
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2022-12-11 22:27:09 +0100

    msg

--- src/main/java/ba/unsa/etf/rpr/PropertyDAO.java
+++ src/main/java/ba/unsa/etf/rpr/PropertyDAO.java
@@ -1,4 +1,10 @@
 package ba.unsa.etf.rpr;
 
+import java.util.List;
+
 public interface PropertyDAO extends Dao<Property>{
+
+    List<Property> searchByType(String type);
+
+    List<Property> searchByPrice(String price);
 }
--- src/main/java/ba/unsa/etf/rpr/ReservationsDAO.java
+++ src/main/java/ba/unsa/etf/rpr/ReservationsDAO.java
@@ -1,6 +1,10 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Date;
+import java.util.List;
+
 public interface ReservationsDAO extends Dao<Reservations>{
+    List<Reservations> nightsOfStay(Date a, Date b);
 
 }
 
