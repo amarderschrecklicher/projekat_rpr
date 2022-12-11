50d82fa894a60816cb2df8da0f3e58afb9cb4160
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2022-12-11 22:23:42 +0100

    msg

--- src/main/java/ba/unsa/etf/rpr/GuestDAO.java
+++ src/main/java/ba/unsa/etf/rpr/GuestDAO.java
@@ -1,5 +1,11 @@
 package ba.unsa.etf.rpr;
 
+import java.util.List;
+
 public interface GuestDAO extends Dao<Guest>{
+
+    List<Guest> searchByName(String name);
+
+    List<Guest> searchByEmail(String email);
 
 }
