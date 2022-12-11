8ad96224ced162c34fa65e0fdbe2eff8c7bfd220
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2022-12-11 22:20:25 +0100

    msg

--- src/main/java/ba/unsa/etf/rpr/HostDAO.java
+++ src/main/java/ba/unsa/etf/rpr/HostDAO.java
@@ -1,5 +1,11 @@
 package ba.unsa.etf.rpr;
 
+import java.util.List;
+
 public interface HostDAO extends Dao<Host>{
+
+    List<Host> searchByName(String name);
+
+    List<Host> searchByNumber(String name);
 
 }
