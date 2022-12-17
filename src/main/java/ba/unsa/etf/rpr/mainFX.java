fe4c1731f67013c3d89116faa24ccf53661b2bb0
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2022-12-17 22:15:51 +0100

    msg

--- src/main/java/ba/unsa/etf/rpr/mainFX.java
+++ src/main/java/ba/unsa/etf/rpr/mainFX.java
@@ -8,18 +8,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 public class mainFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader= new FXMLLoader(getClass().getResource("fxmly.fxml"));
-        Parent root = null;
-        Controller controller = new Controller();
-        loader.setController(controller);
-        root = loader.load();
-        Scene scene =new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+        Parent root=FXMLLoader.load(getClass().getResource("fxmly.fxml"));
         stage.setTitle("JAVAFX");
-        stage.setScene(scene);
+        stage.setScene(new Scene(root , 300,400));
         stage.show();
     }
     public static void mainFX(String []args) {
