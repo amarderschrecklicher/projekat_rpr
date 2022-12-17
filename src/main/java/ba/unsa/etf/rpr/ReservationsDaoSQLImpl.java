119af5dcb53748b4cb25bfa083ed43dbf80a8d02
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2022-12-17 15:05:15 +0100

    msg

--- src/main/java/ba/unsa/etf/rpr/ReservationsDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/ReservationsDaoSQLImpl.java
@@ -95,22 +95,29 @@ public class ReservationsDaoSQLImpl implements ReservationsDAO{
 
     @Override
     public Property propertyDetails(int idReservation) {
-        String query = "SELECT * FROM Reservations WHERE GuestID = ?";
+        String query = "SELECT * FROM Reservations WHERE idReservationss = ?";
         try {
             PreparedStatement stmt = this.connection.prepareStatement(query);
             stmt.setInt(1, idReservation);
             ResultSet rs = stmt.executeQuery();
-            List<Reservations> res = null;
             if(rs.next()) {
-                Reservations rez=new Reservations();
-                rez.setId(rs.getInt(1));
-                rez.setHostID(rs.getInt(2));
-                rez.setGuestID(rs.getInt(3));
-                rez.setPropertyID(rs.getInt(4));
-                rez.setReservationDate(rs.getDate(5));
-                rez.setDateIn(rs.getDate(6));
-                rez.setDateOut(rs.getDate(7));
-                res.add(rez);
+            query="SELECT * FROM Property WHERE PropertyID = ?";
+            stmt = this.connection.prepareStatement(query);
+            stmt.setInt(1, rs.getInt(4));
+            ResultSet rs2=stmt.executeQuery();
+            if(rs2.next()) {
+                Property property =new Property();
+                property.setId(rs2.getInt(1));
+                property.setPropertyType(rs2.getString(2));
+                property.setCapacity(rs2.getInt(3));
+                property.setBathrooms(rs2.getInt(4));
+                property.setKitchens(rs2.getInt(5));
+                property.setLocation(rs2.getString(6));
+                property.setAcH(rs2.getBoolean(7));
+                property.setPrice(rs2.getDouble(8));
+                return property;
+            }
+
             }
             else {
                 return null; // if there is no elements in the result set return null
@@ -125,6 +132,35 @@ public class ReservationsDaoSQLImpl implements ReservationsDAO{
 
     @Override
     public Host hostOfReservation(int idReservation) {
+        String query = "SELECT * FROM Reservations WHERE idReservationss = ?";
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setInt(1, idReservation);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()) {
+                query="SELECT * FROM Host WHERE idHost = ?";
+                stmt = this.connection.prepareStatement(query);
+                stmt.setInt(1, rs.getInt(2));
+                ResultSet rs2=stmt.executeQuery();
+                if(rs2.next()) {
+                    Host host =new Host();
+                    host.setId(rs2.getInt(1));
+                    host.setName(rs2.getString(2));
+                    host.setNumber(rs2.getString(3));
+                    host.setEmail(rs2.getString(4));
+                    return host;
+                }
+
+            }
+            else {
+                return null; // if there is no elements in the result set return null
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace(); // poor error handling
+        }
+
+
         return null;
     }
 
