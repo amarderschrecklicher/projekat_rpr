d16fa17f418a2a4d85b7be1cf327bcdba84e3556
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2022-12-24 15:17:13 +0100

    msg

--- src/main/java/ba/unsa/etf/rpr/dao/GuestDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GuestDAO.java
@@ -1,11 +1,12 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Guest;
+import ba.unsa.etf.rpr.exceptions.Exceptionss;
 
 public interface GuestDAO extends Dao<Guest> {
 
-    int numberOfGuests(String name);
+    int numberOfGuests(String name) throws Exceptionss;
 
-    String countryOfOrigin(String email);
+    String countryOfOrigin(String email) throws Exceptionss;
 
 }
