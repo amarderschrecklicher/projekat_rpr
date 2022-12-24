404acccd9cc3b42eae7e547db5821d5ddb001bb8
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2022-12-24 15:17:35 +0100

    msg

--- src/main/java/ba/unsa/etf/rpr/dao/PropertyDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PropertyDAO.java
@@ -1,16 +1,17 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Property;
+import ba.unsa.etf.rpr.exceptions.Exceptionss;
 
 import java.util.Date;
 
 public interface PropertyDAO extends Dao<Property> {
 
-   boolean hasAC(int idProperty);
+   boolean hasAC(int idProperty) throws Exceptionss;
 
-   double pricePerNight(int idProperty);
+   double pricePerNight(int idProperty) throws Exceptionss;
 
-   boolean isAvailable(Property property, Date d1,Date d2);
+   boolean isAvailable(Property property, Date d1,Date d2) throws Exceptionss;
 
 
 }
