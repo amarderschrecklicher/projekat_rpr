a2a9edd125f49130cb4b140c5fed89afd81f00be
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2022-12-24 15:17:59 +0100

    msg

--- src/main/java/ba/unsa/etf/rpr/dao/ReservationsDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationsDAO.java
@@ -4,19 +4,20 @@ import ba.unsa.etf.rpr.domain.Guest;
 import ba.unsa.etf.rpr.domain.Host;
 import ba.unsa.etf.rpr.domain.Property;
 import ba.unsa.etf.rpr.domain.Reservations;
+import ba.unsa.etf.rpr.exceptions.Exceptionss;
 
 import java.util.List;
 
 public interface ReservationsDAO extends Dao<Reservations> {
     int nightsOfStay(int idReservation);
 
-    List<Reservations> allOfReseravtions(Guest guest);
+    List<Reservations> allOfReseravtions(Guest guest)throws Exceptionss;
 
-    Property propertyDetails(int idReservation);
+    Property propertyDetails(int idReservation)throws Exceptionss;
 
-    Host hostOfReservation(int idReservation);
+    Host hostOfReservation(int idReservation)throws Exceptionss;
 
-    boolean  capacityMatch(int idReservation);
+    boolean  capacityMatch(int idReservation)throws Exceptionss;
 
 }
 
