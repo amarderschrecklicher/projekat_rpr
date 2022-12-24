8f77d774f9be7d7dbe9d22e72d9d4f92d1cb15ab
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2022-12-24 18:52:27 +0100

    msg

--- src/main/java/ba/unsa/etf/rpr/dao/GuestDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GuestDAO.java
@@ -3,10 +3,12 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Guest;
 import ba.unsa.etf.rpr.exceptions.Exceptionss;
 
+import java.util.List;
+
 public interface GuestDAO extends Dao<Guest> {
 
-    int numberOfGuests(String name) throws Exceptionss;
+    List<Guest> searchByName(String name) throws Exceptionss;
 
-    String countryOfOrigin(String email) throws Exceptionss;
+    List<Guest> searchByCountryOfOrigin(String email) throws Exceptionss;
 
 }
--- src/main/java/ba/unsa/etf/rpr/dao/GuestDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GuestDaoSQLImpl.java
@@ -10,9 +10,8 @@ import java.util.TreeMap;
 
 public class GuestDaoSQLImpl extends AbstractDao<Guest> implements GuestDAO {
 
-    private Connection connection;
 
-    public GuestDaoSQLImpl(){
+    public GuestDaoSQLImpl() {
         super("Guest");
     }
 
@@ -37,48 +36,20 @@ public class GuestDaoSQLImpl extends AbstractDao<Guest> implements GuestDAO {
         Map<String, Object> row = new TreeMap<String, Object>();
         row.put("id", object.getId());
         row.put("name", object.getName());
-        row.put("numberOfGuests",object.getNumberOfGuests());
-        row.put("number",object.getGuestNumber());
-        row.put("country",object.getCountry());
+        row.put("numberOfGuests", object.getNumberOfGuests());
+        row.put("number", object.getGuestNumber());
+        row.put("country", object.getCountry());
         return row;
     }
+@Override
+ public List<Guest>searchByName(String name) throws Exceptionss {
+     return executeQuery("SELECT GuestCountry FROM guest WHERE GuestEmail = ?", new Object[]{name});
+
+ }
 
     @Override
-    public int numberOfGuests(String name) {
-        String query = "SELECT numberofguests FROM guest WHERE GuestName = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setString(1, name);
-            ResultSet rs = stmt.executeQuery();
+    public List<Guest> searchByCountryOfOrigin(String email) throws Exceptionss {
 
-            if (rs.next()) { // result set is iterator.
-                return rs.getInt(3);
-            }else {
-                return 0; // if there is no elements in the result set return null
-            }
-        } catch (SQLException e) {
-            e.printStackTrace(); // poor error handling
-        }
-
-        return 0;
-    }
-
-    @Override
-    public String countryOfOrigin(String email) { String query = "SELECT GuestCountry FROM guest WHERE GuestEmail = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setString(1, email);
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) { // result set is iterator.
-                return rs.getString(4);
-            }else {
-                return "No guest"; // if there is no elements in the result set return null
-            }
-        } catch (SQLException e) {
-            e.printStackTrace(); // poor error handling
-        }
-
-        return "No guest";
+        return executeQuery("SELECT GuestCountry FROM guest WHERE GuestEmail = ?", new Object[]{email});
     }
 }
