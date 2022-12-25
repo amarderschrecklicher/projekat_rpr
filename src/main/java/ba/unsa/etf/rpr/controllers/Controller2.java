d5c9e9cc6796cf38bfe28fd7609806f3ac63f3a7
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2022-12-25 08:55:06 +0100

    msg

--- src/main/java/ba/unsa/etf/rpr/controllers/Controller2.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Controller2.java
@@ -32,7 +32,48 @@ public class Controller2 {
     @FXML
     private GridPane scenePanee;
 
-    public void userName(ActionEvent actionEvent) {
+    @FXML
+    public void initialize() {
+
+        nameHost.textProperty().addListener((obs,stara,nova)->{
+            if(nameHost.getText().isEmpty()){
+                nameHost.getStyleClass().removeAll("Ispravno");
+                nameHost.getStyleClass().add("Neispravno");
+            }else{
+                nameHost.getStyleClass().removeAll("Neispravno");
+                nameHost.getStyleClass().add("Ispravno");
+            }
+        });
+
+        surnameHost.textProperty().addListener((obs,stara,nova)->{
+            if(surnameHost.getText().isEmpty()){
+                surnameHost.getStyleClass().removeAll("Ispravno");
+                surnameHost.getStyleClass().add("Neispravno");
+            }else{
+                surnameHost.getStyleClass().removeAll("Neispravno");
+                surnameHost.getStyleClass().add("Ispravno");
+            }
+        });
+
+        numberHost.textProperty().addListener((obs,stara,nova)->{
+            if(numberHost.getText().isEmpty()){
+                numberHost.getStyleClass().removeAll("Ispravno");
+                numberHost.getStyleClass().add("Neispravno");
+            }else{
+                numberHost.getStyleClass().removeAll("Neispravno");
+                numberHost.getStyleClass().add("Ispravno");
+            }
+        });
+
+        emailHost.textProperty().addListener((obs,stara,nova)->{
+            if(emailHost.getText().isEmpty()){
+                emailHost.getStyleClass().removeAll("Ispravno");
+                emailHost.getStyleClass().add("Neispravno");
+            }else{
+                emailHost.getStyleClass().removeAll("Neispravno");
+                emailHost.getStyleClass().add("Ispravno");
+            }
+        });
     }
 
 
