027ed3aee1c9c84de3ff8219f43d7fe5c6441303
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2022-12-25 09:00:03 +0100

    msg

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/Controller3.java
@@ -0,0 +1,70 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.dao.*;
+import ba.unsa.etf.rpr.domain.Host;
+import ba.unsa.etf.rpr.exceptions.Exceptionss;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.Date;
+import java.util.List;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+
+public class Controller3 {
+
+    @FXML
+    public void initialize() {
+/*
+        nameHost.textProperty().addListener((obs,stara,nova)->{
+            if(nameHost.getText().isEmpty()){
+                nameHost.getStyleClass().removeAll("Ispravno");
+                nameHost.getStyleClass().add("Neispravno");
+            }else{
+                nameHost.getStyleClass().removeAll("Neispravno");
+                nameHost.getStyleClass().add("Ispravno");
+            }
+        });
+
+        surnameHost.textProperty().addListener((obs,stara,nova)->{
+            if(surnameHost.getText().isEmpty()){
+                surnameHost.getStyleClass().removeAll("Ispravno");
+                surnameHost.getStyleClass().add("Neispravno");
+            }else{
+                surnameHost.getStyleClass().removeAll("Neispravno");
+                surnameHost.getStyleClass().add("Ispravno");
+            }
+        });
+
+        numberHost.textProperty().addListener((obs,stara,nova)->{
+            if(numberHost.getText().isEmpty()){
+                numberHost.getStyleClass().removeAll("Ispravno");
+                numberHost.getStyleClass().add("Neispravno");
+            }else{
+                numberHost.getStyleClass().removeAll("Neispravno");
+                numberHost.getStyleClass().add("Ispravno");
+            }
+        });
+
+        emailHost.textProperty().addListener((obs,stara,nova)->{
+            if(emailHost.getText().isEmpty()){
+                emailHost.getStyleClass().removeAll("Ispravno");
+                emailHost.getStyleClass().add("Neispravno");
+            }else{
+                emailHost.getStyleClass().removeAll("Neispravno");
+                emailHost.getStyleClass().add("Ispravno");
+            }
+        });
+   */ }
+
+
+
+}
