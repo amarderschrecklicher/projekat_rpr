4eb0c6effa3d915d733049c57df2cddfe455c73f
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2023-01-12 20:02:37 +0100

    msg

--- src/main/java/ba/unsa/etf/rpr/controllers/PropertyController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PropertyController.java
@@ -124,15 +124,7 @@ public class PropertyController implements Initializable {
                 location.getStyleClass().removeAll("Neispravno");
                 location.getStyleClass().add("Ispravno");
             }
-        });        propertyNname.textProperty().addListener((obs,stara,nova)->{
-            if(propertyNname.getText().isEmpty()){
-                propertyNname.getStyleClass().removeAll("Ispravno");
-                propertyNname.getStyleClass().add("Neispravno");
-            }else{
-                propertyNname.getStyleClass().removeAll("Neispravno");
-                propertyNname.getStyleClass().add("Ispravno");
-            }
-        });        propertyNname.textProperty().addListener((obs,stara,nova)->{
+        });       propertyNname.textProperty().addListener((obs,stara,nova)->{
             if(propertyNname.getText().isEmpty()){
                 propertyNname.getStyleClass().removeAll("Ispravno");
                 propertyNname.getStyleClass().add("Neispravno");
