0b58e25cca1c8b84e5b4eb7e0092e3eb4167ebb7
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2023-02-11 23:18:41 +0100

    add date

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/ReservationsController.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReservationsController extends HostController implements Initializable {
+
+    public  Label AC ;
+    public Label propertyName;
+    public TableView table;
+    public Label location;
+    public Label capacity;
+    public Label price;
+
+    @FXML
+    public void cancelButton(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void applyButton(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
